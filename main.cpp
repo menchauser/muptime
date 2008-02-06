31351e01481877e1a14610b992cf0baf111355dc
menchauser <menchauser@0ed3d430-1e45-0410-b51e-73e0f13d323a>
2008-02-06 10:57:31 +0000

    all in Unicode added settings

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 /**
  * (c) Muhamed Karanashev, 2008
  */
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <windows.h>
 #include <tchar.h>
 #include "resource.h"
 
-#define ADDTIMEPART(time, str)        if (strlen(str) <= 1) strcat(time, "0\0"); strcat(time, str);
+#define ADDTIMEPART(time, str)        if (wcslen(str) <= 1) wcscat(time, _T("0\0")); wcscat(time, str);
 #define UCALLBACKMESSAGE 0x1001
 
 static TCHAR szWindowClass[] = _T("uptimeAppClass");
 static TCHAR szTitle[] = _T("muptime");
 static BOOL isMinimized = FALSE;
+static UINT POS_X = 0;
+static UINT POS_Y = 48;
+static wchar_t iniFile[MAX_PATH];
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-char* getUptimeStr(char*);
+wchar_t* getUptimeStr(wchar_t*);
 void setFont(LOGFONT*);
-void copyTextToClipboard(HWND, char*);
+void copyTextToClipboard(HWND, wchar_t*);
+void readSettingsFromFile(LPCTSTR fileName);
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR cmdLine, int nCmdShow) {
     WNDCLASSEX wnd;
@@ -40,12 +45,16 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR cmdLine, int nCmd
         return 1;
     }
 
+    GetCurrentDirectory(MAX_PATH, iniFile);
+    wcscat_s(iniFile, _T("\\muptime.ini"));
+    POS_X = GetPrivateProfileInt(_T("Position"), _T("X"), POS_X, iniFile);
+    POS_Y = GetPrivateProfileInt(_T("Position"), _T("Y"), POS_Y, iniFile);
     HWND hWnd = CreateWindowEx(
         WS_EX_TOOLWINDOW,
         szWindowClass,
         szTitle,
         WS_POPUP | WS_DLGFRAME, //WS_DLGFRAME
-        0, 48,
+        POS_X, POS_Y,
         96, 25,
         NULL,
         NULL,
@@ -60,7 +69,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR cmdLine, int nCmd
     }
     ShowWindow(hWnd, nCmdShow);
     UpdateWindow(hWnd);
-    SetWindowPos(hWnd, HWND_TOPMOST, 0,0,0,0, SWP_NOMOVE|SWP_NOSIZE);
+    SetWindowPos(hWnd, HWND_TOPMOST, POS_X, POS_Y, 0, 0, SWP_NOSIZE);
     MSG msg;
     {
         NOTIFYICONDATA ntdata;
@@ -70,7 +79,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR cmdLine, int nCmd
         ntdata.uFlags = NIF_MESSAGE | NIF_TIP | NIF_ICON;
         ntdata.uCallbackMessage = UCALLBACKMESSAGE;
         ntdata.hIcon = LoadIconA(GetModuleHandleA(NULL),MAKEINTRESOURCEA(IDI_ICON1));
-        strcpy(ntdata.szTip, "muptime");
+        wcscpy_s(ntdata.szTip, _T("muptime"));
         Shell_NotifyIcon(NIM_ADD, &ntdata);
     }
     while (GetMessage(&msg, NULL, 0, 0)) {
@@ -82,7 +91,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR cmdLine, int nCmd
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wparam, LPARAM lparam) {
     const UINT_PTR IDT_TIMER1 = 1;
-    char *time = new char[16];
+    wchar_t *time = new wchar_t[16];
     LOGFONT lg;
     HFONT fnt;
     PAINTSTRUCT ps;
@@ -94,7 +103,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wparam, LPARAM lparam) 
             fnt = CreateFontIndirect(&lg);
             SelectObject(hdc, fnt);
             getUptimeStr(time);
-            TextOutA(hdc, 3, 3, time, strlen(time));
+            TextOutW(hdc, 3, 3, time, wcslen(time));
             EndPaint(hWnd, &ps);
             DeleteObject(fnt);
             break;
@@ -102,6 +111,17 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wparam, LPARAM lparam) 
             SetTimer(hWnd, IDT_TIMER1, 1000, (TIMERPROC)NULL);
             break;
         case WM_DESTROY:
+            {
+                LPRECT rect = new tagRECT;
+                GetWindowRect(hWnd, rect);
+                wchar_t x_pos[4];
+                wchar_t y_pos[4];
+                _ltow(rect->left, x_pos, 10);
+                _ltow(rect->top, y_pos, 10);
+                WritePrivateProfileString(_T("Position"), _T("X"), x_pos, iniFile);
+                WritePrivateProfileString(_T("Position"), _T("Y"), y_pos, iniFile);
+                delete rect;
+            }
             KillTimer(hWnd, IDT_TIMER1);
             PostQuitMessage(0);
             break;
@@ -111,7 +131,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wparam, LPARAM lparam) 
             fnt = CreateFontIndirect(&lg);
             SelectObject(hdc, fnt);
             getUptimeStr(time);
-            TextOutA(hdc, 3, 3, time, strlen(time));
+            TextOutW(hdc, 3, 3, time, wcslen(time));
             DeleteObject(fnt);
             ReleaseDC(hWnd, hdc);
             break;
@@ -141,7 +161,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wparam, LPARAM lparam) 
                 ntdata.uFlags = NIF_MESSAGE | NIF_TIP | NIF_ICON;
                 ntdata.uCallbackMessage = UCALLBACKMESSAGE;
                 ntdata.hIcon = LoadIconA(GetModuleHandleA(NULL),MAKEINTRESOURCEA(IDI_ICON1));
-                strcpy(ntdata.szTip, "muptime");
+                wcscpy(ntdata.szTip, _T("muptime"));
                 Shell_NotifyIcon(NIM_ADD, &ntdata);
                 ShowWindow(hWnd, SW_HIDE);
             }
@@ -162,7 +182,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wparam, LPARAM lparam) 
                             ntdata.uFlags = NIF_MESSAGE | NIF_TIP | NIF_ICON;
                             ntdata.uCallbackMessage = UCALLBACKMESSAGE;
                             ntdata.hIcon = LoadIconA(GetModuleHandleA(NULL),MAKEINTRESOURCEA(IDI_ICON1));
-                            strcpy(ntdata.szTip, "muptime");
+                            wcscpy_s(ntdata.szTip, _T("muptime"));
                             Shell_NotifyIcon(NIM_ADD, &ntdata);
                             ShowWindow(hWnd, SW_HIDE);
                             isMinimized = TRUE;
@@ -185,6 +205,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wparam, LPARAM lparam) 
                 ntdata.uCallbackMessage = UCALLBACKMESSAGE;
                 Shell_NotifyIcon(NIM_DELETE, &ntdata);
             }
+            
             break;
         default:
             return DefWindowProc(hWnd, message, wparam, lparam);
@@ -194,7 +215,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wparam, LPARAM lparam) 
     return 0;
 }
 
-char* getUptimeStr(char* dst) {
+wchar_t* getUptimeStr(wchar_t* dst) {
     long milliseconds = GetTickCount();
     long days = milliseconds / 86400000;
     milliseconds -= days * 86400000;
@@ -204,35 +225,35 @@ char* getUptimeStr(char* dst) {
     milliseconds -= minutes * 60000;
     long seconds = milliseconds / 1000;
     
-    char* time = new char[16];
+    wchar_t* time = new wchar_t[16];
     memset(time, 0, 16);
 
-    char* sDays = new char[3];
-    sDays = ltoa(days, sDays, 10);
+    wchar_t* sDays = new wchar_t[3];
+    sDays = _ltow(days, sDays, 10);
     ADDTIMEPART(time, sDays);
-    strcat(time, ":\0");
+    wcscat(time, _T(":\0"));
     delete[] sDays;
-    char* sHours = new char[3];
-    sHours = ltoa(hours, sHours, 10);
+    wchar_t* sHours = new wchar_t[3];
+    sHours = _ltow(hours, sHours, 10);
     ADDTIMEPART(time, sHours);
-    strcat(time, ":\0");
+    wcscat(time, _T(":\0"));
     delete[] sHours;
-    char* sMinutes = new char[3];
-    sMinutes = ltoa(minutes, sMinutes, 10);
+    wchar_t* sMinutes = new wchar_t[3];
+    sMinutes = _ltow(minutes, sMinutes, 10);
     ADDTIMEPART(time, sMinutes);
-    strcat(time, ":\0");
+    wcscat(time, _T(":\0"));
     delete[] sMinutes;
-    char* sSeconds = new char[3];
-    sSeconds = ltoa(seconds, sSeconds, 10);
+    wchar_t* sSeconds = new wchar_t[3];
+    sSeconds = _ltow(seconds, sSeconds, 10);
     ADDTIMEPART(time, sSeconds);
     delete[] sSeconds;
-    strncpy(dst, time, 16);
+    wcsncpy(dst, time, 16);
     delete[] time;
     return dst;
 }
 
 void setFont(LOGFONT *lg) {
-    const char strName[] = "SomeUptimeFont";
+    const wchar_t strName[] = _T("SomeUptimeFont");
     lg->lfHeight = 14;
     lg->lfWidth  = 8;
     lg->lfEscapement = 0;
@@ -246,14 +267,18 @@ void setFont(LOGFONT *lg) {
     lg->lfClipPrecision = CLIP_DEFAULT_PRECIS;
     lg->lfQuality = PROOF_QUALITY;
     lg->lfPitchAndFamily = DEFAULT_PITCH | FF_DONTCARE;
-    strcpy(lg->lfFaceName, strName);
+    wcscpy_s(lg->lfFaceName, strName);
 }
 
-void copyTextToClipboard(HWND hWnd, char* src) {
+void copyTextToClipboard(HWND hWnd, wchar_t* src) {
     //копируем src в буфер окна hWnd
     if(OpenClipboard(hWnd)){
         EmptyClipboard();
         SetClipboardData(CF_TEXT, (HANDLE)src);
         CloseClipboard();
     }
+}
+
+void readSettingsFromFile(LPCTSTR fileName) {
+
 }
